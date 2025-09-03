7896e89e35a991b1a6a6712d2deaf669aceb99da
Anas Ibrahim <anas.ibrahim@the-doable.com>
2025-09-03 16:19:36 +0300

    Delete And Setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,8 @@
-import "./globals.css";
-import { Cabin } from 'next/font/google';
-import ClientLayout from '@/components/ClientLayout';
-
-const cabin = Cabin({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
-});
-
-export default function RootLayout() {
+export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
-    <html lang="en">
-      <body className={cabin.className}>
-        <ClientLayout />
+    <html>
+      <body>
+        {children}
       </body>
     </html>
   );
