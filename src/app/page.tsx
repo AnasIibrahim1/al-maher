869f175261131ca92423a8a3c1914be4146c94d9
Anas Ibrahim <anas.ibrahim@the-doable.com>
2025-09-04 11:13:45 +0300

    BackBoxCard Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import General_Button from '../components/Buttons/GeneralButtons/Buttons';
 import SocialButtons from '../components/Buttons/Solcial Buttons/SocialButtons';
+import BackBoxCard from '../components/Cards/BackBoxCard/BackBoxCard';
 import './page.css';  
 
 export default function Home() {
@@ -11,6 +12,13 @@ export default function Home() {
         إبحث عن دروسك !
       </General_Button>
       <SocialButtons backgroundColor="transparent" borderColor="#fff" />
+      <BackBoxCard content={
+        <div>
+          <h1>Back Box Card</h1>
+          <p>Back Box Card</p>
+          <p>Back Box Card</p>
+        </div>  
+      } image="https://images.unsplash.com/photo-1534796636912-3b95b3ab5986?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" backgroundColor="#4F5DE4" />
     </div>
   );
 }
