27001e2ca22b2066f07dac9a9151f090b25d21f7
Anas Ibrahim <anas.ibrahim@the-doable.com>
2025-09-04 11:24:27 +0300

    Icons Buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import General_Button from '../components/Buttons/GeneralButtons/Buttons';
 import SocialButtons from '../components/Buttons/Solcial Buttons/SocialButtons';
 import BackBoxCard from '../components/Cards/BackBoxCard/BackBoxCard';
+import PaperCard from '../components/Cards/PaperCard/PaperCard';
+import IconButtons from '../components/Buttons/IconButtons/IconButtons';
 import './page.css';  
 
 export default function Home() {
@@ -19,6 +21,10 @@ export default function Home() {
           <p>Back Box Card</p>
         </div>  
       } image="https://images.unsplash.com/photo-1534796636912-3b95b3ab5986?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" backgroundColor="#4F5DE4" />
+      <PaperCard />
+      <IconButtons backgroundColor="#4F5DE4" borderRadius="10px" width="fit-content" height="fit-content" padding="20px" icon={
+        <img src="./Group.svg" alt="Icon" />
+      } />
     </div>
   );
 }
---
+++ src/components/Cards/PaperCard/PaperCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './PaperCard.css';
+
+export default function PaperCard() {
+  return (
+<div className="paper-card-container-general">
+<div className="paper-card-container">
+        <div className="paper-card-content">
+            <h1>Paper Card</h1>
+        </div>
+    </div>
+</div>
+  )
+}
