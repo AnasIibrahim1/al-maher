d53bb6ee3648e82c10ebed672826c463255301de
Anas Ibrahim <anas.ibrahim@the-doable.com>
2025-09-07 11:12:35 +0300

    Backcircle Animations

--- src/components/Circles/BackCircles/BackCircles.jsx
+++ src/components/Circles/BackCircles/BackCircles.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import './BackCircles.css'
 
-export default function BackCircles({ width, height, style, content}) {
+export default function BackCircles({ width, height, style, content, rotationSpeed = 20}) {
+  const containerStyle = {
+    width: width,
+    height: height,
+  };
+
+  const contentStyle = {
+    backgroundColor: "blue",
+    width: "70%",
+    height: "70%",
+  };
+
   return (
-    <div className="back-circles-container" style={{ width: width, height: height, position: "relative" }}>
-      <img src="backcircle.svg" alt="back-circles" style={{width: "100%", height: "100%", objectFit: "cover", ...style}}/>
-      <div style={{position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", backgroundColor: "blue", width: "70%", height: "70%", zIndex: "1", borderRadius: "50%", padding: "50px"}}>
+    <div className="back-circles-container" style={containerStyle}>
+      <img 
+        src="backcircle.svg" 
+        alt="back-circles" 
+        className="back-circles-image"
+        style={{
+          animationDuration: `${rotationSpeed}s`,
+          ...style
+        }}
+      />
+      <div className="back-circles-content" style={contentStyle}>
         {content}
       </div>
     </div>
